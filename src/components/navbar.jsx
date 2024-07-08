28d8fa9ae412f2b430399c2d76d6935ef0b58c74
Mabel Aikins Acquah <acquahmabel60@gmail.com>
2024-07-08 13:32:57 +0000

    New Changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,8 @@ import K from "../constant";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between px-40 py-10 fixed w-full left-0 top-0 text-white">
+    <div>
+ <div className="flex justify-between px-40 py-10 fixed w-full left-0 top-0 text-white">
       <span className="text-black font-extrabold font-mono italic text-[1.2rem]">BelDrive</span>
       <div className="flex gap-x-10">
         {
@@ -16,6 +17,8 @@ const Navbar = () => {
         }
       </div>
     </div>
+    </div>
+   
   )
 };
 
--- src/pages/landing/components/collection.jsx
+++ src/pages/landing/components/collection.jsx
@@ -13,13 +13,16 @@ const Collection = () => {
       <h5 className="text-gray-300 font-bold">OUR CLASSIC CARS</h5>
       <h3 className="font-bold text-[30px] text-blue-950">Car Collections</h3>
       </div>
-      <div className="grid grid-cols-3 mt-8 gap-y-7">
-      <img src={imageOne} alt="image one" />
-        <img src={imageTwo} alt="" />
-        <img src={imageThree}alt="" />
-        <img src={imageFour} alt="" />
-        <img src={imageFive} alt="" />
-        <img src={imageSix}alt="" />
+      <div className="grid grid-cols-4 mt-8 gap-y-7 pl-10 pr-10 shadow-xl p-10">
+      <img src={imageOne} alt="image one"  className="transform transition-transform duration-500 hover:scale-125"/>
+        <img src={imageTwo} alt=""  className="transform transition-transform duration-500 hover:scale-125"/>
+        <img src={imageThree}alt="" className="transform transition-transform duration-500 hover:scale-125"/>
+        <img src={imageTwo} alt="" className="transform transition-transform duration-500 hover:scale-125"/>
+        <img src={imageFive} alt="" className="transform transition-transform duration-500 hover:scale-125"/>
+        <img src={imageSix}alt="" className="transform transition-transform duration-500 hover:scale-125"/>
+        <img src={imageOne} alt="" className="transform transition-transform duration-500 hover:scale-125"/>
+        <img src={imageTwo} alt=""  className="transform transition-transform duration-500 hover:scale-125"/>
+        
       </div>
       
     </div>
